8a563a82cbd4841a0977827cfb27868ca1aa463f
Pablo González <pablogonzaleztemes@gmail.com>
2026-05-12 02:19:05 +0200

    feat(UfosPark.java): :sparkles: add UfosPark class

---
+++ src/main/java/edu/teamrocket/UfosPark.java
@@ -0,0 +1,11 @@
+package edu.teamrocket;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UfosPark {
+
+    List<String> flota = new ArrayList<>();
+
+    public UfosPark() {}
+}
