c9469392c8762b324987dfdb96e9e170010f29aa
Pablo Gonzalez <pablogonzaleztemes@gmail.com>
2026-05-15 14:13:00 +0200

    feat(payment): :sparkles: add new methods

---
+++ src/main/java/edu/teamrocket/payment/CreditCard.java
@@ -0,0 +1,36 @@
+package edu.teamrocket.payment;
+
+public class CreditCard implements PaymentMethod {
+    private final String owner;
+    private final String number;
+    private double credit = 3000;
+
+    public CreditCard(String owner, String number) {
+        this.owner = owner;
+        this.number = number;
+    }
+
+    @Override
+    public boolean pay(double fee) {
+        boolean isEnough = (this.credit - fee != this.credit);
+        if (isEnough) {
+            this.credit -= fee;
+            return isEnough;
+        } else {
+            return isEnough;
+        }
+    }
+
+    @Override
+    public String cardNumbers() {
+        return this.number;
+    }
+
+    public double credit() {
+        return this.credit;
+    }
+
+    public String cardOwner() {
+        return this.owner;
+    }
+}
