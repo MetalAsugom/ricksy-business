1d18a4c40152003c81cdf4924881195195339984
Xián Morales Fonseca <xianmorales01@gmail.com>
2026-05-16 19:41:26 +0200

    feat(dispatches): :sparkles: Finish CrystalExpenders class

--- src/main/java/edu/teamrocket/dispatchers/CrystalExpender.java
+++ src/main/java/edu/teamrocket/dispatchers/CrystalExpender.java
@@ -1,5 +1,39 @@
 package edu.teamrocket.dispatchers;
 
-public class CrystalExpender {
-    
+import edu.teamrocket.payment.PaymentMethod;
+import edu.teamrocket.receptivo.GuestDispatcher;
+
+public class CrystalExpender implements GuestDispatcher{
+    private int stock;
+    private double itemCost;
+
+    public CrystalExpender(int stock, double itemCost) {
+        this.stock = stock;
+        this.itemCost = itemCost;
+    }
+
+    @Override
+    public void dispatch(PaymentMethod usuario) {
+        if(this.stock > 0 && usuario.pay(getItemCost())) {
+            this.stock -= 1;
+        }
+    }
+
+    public int stock() {
+        return this.stock;
+    }
+
+    public double getItemCost() {
+        return this.itemCost;
+    }
+
+    @Override
+    public String toString() {
+        return """
+                stock: %d
+                cost: %f
+                """.formatted(stock(), getItemCost());
+    }
+
+
 }
