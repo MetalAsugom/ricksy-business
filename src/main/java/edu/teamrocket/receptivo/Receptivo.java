b9a4640fb22dbac747402d16a8c9795922bb368d
Xián Morales Fonseca <xianmorales01@gmail.com>
2026-05-16 19:42:43 +0200

    feat(receptivo): :sparkles: Add class Receptivo

--- src/main/java/edu/teamrocket/receptivo/Receptivo.java
+++ src/main/java/edu/teamrocket/receptivo/Receptivo.java
@@ -1,5 +1,31 @@
 package edu.teamrocket.receptivo;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.teamrocket.payment.PaymentMethod;
+
 public class Receptivo {
+
+    private static Receptivo instance = null;
+
+    private List<GuestDispatcher> receptivo = new ArrayList<>();
+
+    public Receptivo() {
+    }
+
+    public void registra(GuestDispatcher dispatcher) {
+        receptivo.add(dispatcher);
+    }
+
+    public void dispatch(PaymentMethod usuario) {
+        for(GuestDispatcher receptor : receptivo) {
+            receptor.dispatch(usuario);
+        }
+    }
+
+    public static Receptivo getReceptivo() {
+        return instance = instance == null ? new Receptivo() : instance;
+    }
     
 }
